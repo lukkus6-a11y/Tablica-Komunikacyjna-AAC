35b26061b7fd7ecc8c3980d66b9d287ab4fc1145
lukkus6-a11y <lukkus6@gmail.com>
2026-08-23 17:34:51 +0200

    Dodanie detectSessionInUrl do konfiguracji supabase

--- src/supabase.ts
+++ src/supabase.ts
@@ -3,4 +3,10 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = 'https://fnneoykgqmsdsjamhxmw.supabase.co';
 const supabaseKey = 'sb_publishable_SOx0_zsb2SSBFrwBHvZrhw_MANaEZAN';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true, // Kluczowe dla przechwycenia sesji po powrocie z Google
+  },
+});
