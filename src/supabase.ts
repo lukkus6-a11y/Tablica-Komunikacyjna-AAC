f663e3bd98ddb2b14cb053f06ad604b0292b0fad
lukkus6-a11y <lukkus6@gmail.com>
2026-08-23 16:46:00 +0200

    Usuniecie zmiennych srodowiskowych z supabase.ts na rzecz stalych

--- src/supabase.ts
+++ src/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://fnneoykgqmsdsjamhxmw.supabase.co';
-const supabasePublishableKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'sb_publishable_SOx0_zsb2SSBFrwBHvZrhw_MANaEZAN';
+const supabaseUrl = 'https://fnneoykgqmsdsjamhxmw.supabase.co';
+const supabasePublishableKey = 'sb_publishable_SOx0_zsb2SSBFrwBHvZrhw_MANaEZAN';
 
 export const supabase = createClient(supabaseUrl, supabasePublishableKey, {
   auth: {
